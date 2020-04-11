7c4553c0d7169cf8b3e8e484ed364d0bceeb23ef
rafayet <rafayet@hashkloud.com>
2020-04-11 13:59:35 +0600

    added log to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,11 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
